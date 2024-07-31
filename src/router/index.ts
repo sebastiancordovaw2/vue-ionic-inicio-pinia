c39255ec5a3f78b52298fa50d607e48740fb8834
sebastiancordovaw2 <sebastiancordovaw@gmail.com>
2024-07-31 13:35:50 -0400

    ultimo 1

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,7 +27,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory("vue-ionic-inicio-pinia/#"),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
