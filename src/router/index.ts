8cdc29ed044eea286ff486435a20c54c60d272e8
sebastiancordovaw2 <sebastiancordovaw@gmail.com>
2024-07-31 12:20:47 -0400

    agregar config base

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,7 +27,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("#"),
   routes
 })
 
